0a2a50cfc291e3184293cdc850d68528f8b0476d
Daniel Burke <dmhburke@Daniels-MacBook-Air.local>
2019-06-25 19:55:57 -0700

    fix user error

--- hotSpotsApp/settings.py
+++ hotSpotsApp/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['www.dbdevsite.com','db-hotspots.herokuapp.com','127.0.0.1',]
 
